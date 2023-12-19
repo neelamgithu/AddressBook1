d934096d2087bb5818966b8b6213d0dfc2e14bc8
Neelam <neelammujawar12@gmail.com>
2023-12-19 14:32:59 +0530

    Contact created

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,4 +1,41 @@
- class AddressBookMain{
+ import java.util.ArrayList;
+import java.util.Scanner;
+
+
+class Contacts {
+    
+    String first_name  ;
+    String last_name;
+    String address;
+    String city;
+    String state;
+    int zip;
+    String phone_number;
+    String email;
+
+
+    // initialising value with constructor a
+    public Contacts(String first_name, String last_name, String address, String city, String state, int zip,
+                    String phone_number, String email) {
+//this is a key word which is used to refer current object
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phone_number = phone_number;
+        this.email = email;
+
+                    }
+                }
+ 
+ 
+ 
+ public class AddressBookMain{
+    static Scanner sc = new Scanner(System.in);
+    static ArrayList<Contacts> con = new ArrayList<Contacts>();
  public static void main(String[] args) {
         System.out.println("welcome to address book program");
- }}
+ }
+}
