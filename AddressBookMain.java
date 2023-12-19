eba57d87169172f2b9cf2c05a3d007793dc35859
Neelam <neelammujawar12@gmail.com>
2023-12-19 14:40:53 +0530

    Check and Edit

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -61,4 +61,62 @@ public static void add_contact() {
         Contacts cont = new Contacts(first_name, last_name, address, city, state, zip, phone_number, email);
         con.add(cont);
     }
+    public static void edit_contact() {
+        int count = check();//calling check methods
+
+        if (count == -1) {
+            System.out.println(" invalid name \nplease enter valid name");
+
+        }
+        // flag=1 means name is found and index is store in count varible so we can
+        // update details by index of that contact
+        else {
+
+            System.out.println("enter new address");
+            con.get(count).address = sc.nextLine();
+            System.out.println("enter new city");
+            con.get(count).city = sc.nextLine();
+            System.out.println("enter new state");
+            con.get(count).state = sc.nextLine();
+            System.out.println("enter new zip");
+            con.get(count).zip = sc.nextInt();
+            sc.nextLine();// after taking interger as input /n create ussue for that we are using this
+            // method
+            System.out.println("enter new phone number");
+            con.get(count).phone_number = sc.nextLine();
+            System.out.println("enter new email address");
+            con.get(count).email = sc.nextLine();
+            System.out.println("updation done successfully");
+        }
+    }
+    public static int check() {
+        sc.nextLine();// to avoid /n issue in after taking integer as input
+        int count = 0;
+        // flag variable is to check user input name found or not
+        if (con.size() == 0) {
+            System.out.println(" No contact information to edit  ");
+        } else {
+            // int flag = 0;
+            System.out.println("enter name of person to edit details");
+            // reading name from console
+            String cname = sc.nextLine();
+            // iteration until length of arraylist
+            while (count < con.size()) {
+                // checking name of user input is matches with any name of stored contact
+                if (con.get(count).first_name.equals(cname)) {
+                    System.out.println("name fount successfully");
+
+                    return count;
+                    // as name has been found so no need to iterate break used to go out of loop
+                    // after condition true
+                }
+                count++;
+            }
+
+            // if flag=0 means name is not found and so we are priting message and and stop
+            // execution
+        }
+        return -1;
+    }
+
 }
